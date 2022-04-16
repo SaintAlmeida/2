ad63411d12ca61542ea18f1c7a2769785e0aea25
SaintAlmeida <103289195+SaintAlmeida@users.noreply.github.com>
2022-04-15 20:51:04 -0400

    Commit

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM python:3.10.4
 RUN pip install flask
-COPY example.py /app.py
+COPY Olacloudopss.py /app.py
 CMD ["python","app.py"]
--- Olacloudopss.py
+++ Olacloudopss.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Olá, CloudOpss :)"
+    return "Olá, CloudOpss!! )"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
+    
